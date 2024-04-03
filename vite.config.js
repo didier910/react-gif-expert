34da1a18c4f50b527c2266e6670beb0617c16aed
Jesus Gustavo Medrano Romero <gustavomedrano@GS1826478D01MLP.local>
2024-04-03 11:22:12 -0600

    primera configuracion para PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        display: 'standalone',
+        display_override: ['window-controls-overlay'],
+        name: 'GifExpertApp',
+        description: 'GiftExpertApp como PWA',
+        theme_color: '#19223c',
+        background_color: '#d4d4d4',
+        icons: [
+          {
+            src: 'android-launchericon-72-72.png',
+            sizes: '72x72',
+            type: 'image/png'
+          },
+          {
+            src: 'android-launchericon-192-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'android-launchericon-512-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
+  ],
 })
